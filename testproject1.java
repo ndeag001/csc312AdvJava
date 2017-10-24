1ef1c160a591c6a087a8e4f388a4239ba57b7479
ndeag001 <ndeag001@plattsburgh.edu>
2017-10-24 19:58:11 -0400

    Create testproject1.java
    test file nothing added

---
+++ testproject1.java
@@ -0,0 +1,15 @@
+package csc312;
+
+import static org.junit.Assert.*;
+
+import java.net.UnknownHostException;
+
+import org.junit.After;
+import org.junit.*;
+import org.junit.internal.runners.statements.Fail;
+
+import junit.framework.AssertionFailedError;
+
+public class TestWeek3Lecture {
+	
+}
