d7e7d9ed0d9a63c907e05cae32e4d6556c1a46b3
ndeag001 <ndeag001@plattsburgh.edu>
2017-10-26 13:59:05 -0400

    Update testproject1.java
    test week 3 assignment code but changed the url to get the list of search words

--- testproject1.java
+++ testproject1.java
@@ -1,15 +1,52 @@
 package csc312;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import java.net.UnknownHostException;
+import java.io.IOException;
 
-import org.junit.After;
-import org.junit.*;
-import org.junit.internal.runners.statements.Fail;
+import org.junit.Test;
 
-import junit.framework.AssertionFailedError;
+public class testproject1 {
 
-public class TestWeek3Lecture {
 	
+	@Test
+	public void testValidURL() throws IOException {
+		
+		project1 proj1 = new project1();
+				
+		assertEquals( new Character('c'), proj1.getURL( "https://wordfinder-001.appspot.com/word.txt" ) );
+		
+	}
+	
+//	@Test
+//	public void testServerError() throws IOException {
+//		
+//		project1 proj1 = new project1();
+//				
+//		assertEquals( null, proj1.getURL( "https://wordfinder-001.appspot.com/wordfinder?game=2&pos=Z99" ) );
+//		
+//
+//	}
+//	
+//	@Test
+//	public void testServerForbidden() throws IOException {
+//		
+//		project1 proj1 = new project1();
+//				
+//		assertEquals( null, proj1.getURL( "https://wordfinder-001.appspot.com/wordfinder?game=2&pos=Z88" ) );
+//		
+//	
+//	}
+//	
+//	@Test
+//	public void testInvalidDomain() throws IOException {
+//		
+//		project1 proj1 = new project1();
+//				
+//		assertEquals( null, proj1.getURL( "https://aninvaliddomainname.com" ) );
+//		
+//	
+//	}
+//	
+//
 }
