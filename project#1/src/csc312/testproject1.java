3318f8bf5a1f43e19b604f653aed896b111f982e
ndeag001 <ndeag001@plattsburgh.edu>
2017-10-31 14:08:21 -0400

    I think is what you were going for

---
+++ project#1/src/csc312/testproject1.java
@@ -0,0 +1,81 @@
+package csc312;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class testproject1 {
+
+	
+	@Test
+	public void testValidURL() throws IOException {
+		
+		project1 proj1 = new project1();
+				
+		assertEquals( new Character('c'), proj1.getURL( "https://wordfinder-001.appspot.com/word.txt" ) );
+		
+	}
+	
+
+	@Test
+	public void testHmap() throws IOException {
+		
+		project1 proj1 = new project1();
+		
+		String content = proj1.getURL( "https://wordfinder-001.appspot.com/word.txt" );
+		
+		
+		assertEquals( new Character('c'), proj1.getURL( "https://wordfinder-001.appspot.com/word.txt" ) );
+		
+	}
+	
+	@Test
+	public void testAll() throws IOException {
+		
+		// 1 geturl, gethmap()
+		// variable hmap_board
+		// 2 while(word not found) geturl(letters),  hmap_board.add(letter)
+		
+		
+		project1 proj1 = new project1();
+		
+		String content = proj1.getURL( "https://wordfinder-001.appspot.com/word.txt" );
+		
+		
+		assertEquals( new Character('c'), proj1.getURL( "https://wordfinder-001.appspot.com/word.txt" ) );
+		
+	}
+//	@Test
+//	public void testServerError() throws IOException {
+//		
+//		project1 proj1 = new project1();
+//				
+//		assertEquals( null, proj1.getURL( "https://wordfinder-001.appspot.com/wordfinder?game=2&pos=Z99" ) );
+//		
+//
+//	}
+//	
+//	@Test
+//	public void testServerForbidden() throws IOException {
+//		
+//		project1 proj1 = new project1();
+//				
+//		assertEquals( null, proj1.getURL( "https://wordfinder-001.appspot.com/wordfinder?game=2&pos=Z88" ) );
+//		
+//	
+//	}
+//	
+//	@Test
+//	public void testInvalidDomain() throws IOException {
+//		
+//		project1 proj1 = new project1();
+//				
+//		assertEquals( null, proj1.getURL( "https://aninvaliddomainname.com" ) );
+//		
+//	
+//	}
+//	
+//
+}
